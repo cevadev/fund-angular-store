7e2f3f9a9932c27b002ac4d103c7fe23357cec8a
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-09-27 20:57:36 -0300

    Angular - Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Carlos V.';
+  age = 30;
+  image = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Carlos V.',
+    age: 30,
+    avatar: 'https://source.unsplash.com/random',
+  };
+
+  toogleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increase() {
+    this.person.age += 1;
+  }
 }
